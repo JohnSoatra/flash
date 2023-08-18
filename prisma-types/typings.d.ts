aadb9a39884d6d5c3d4a7f112de2ed041978bef1
John Soatra <johnsoatra@gmail.com>
2023-08-18 14:36:59 +0700

    ok

---
+++ prisma-types/typings.d.ts
@@ -0,0 +1,181 @@
+import { Brand, Card, Category, Collection, Image, Model, Order, Payment, Poster, Product, ProductOrder, Shipping, User, Video } from "./index";
+import { JwtPayload } from "jsonwebtoken";
+
+// XType
+// -> Product
+type ProductX = Product & {
+    images: Image[],
+    videos: VideoX[],
+    loved?: boolean
+}
+
+type ProductOrderX = ProductOrder & {
+    product: ProductX
+}
+  
+type OrderX = Order & {
+    product_orders: ProductOrderX[],
+    payment: Payment|null,
+    shipping: Shipping|null
+}
+
+// -> Video
+type VideoX = Video & {
+    poster: Poster|null
+}
+
+// -> Card
+type CardX = Card & {
+    products: ProductX[]
+}
+
+// CType
+// -> User
+type UserC = {
+    id: string,
+    username: string,
+    email: string,
+    
+    fullname: string,
+    phone_number: string,
+    address: string,
+
+    image_url: string|null,
+    image_color: string,
+}
+
+// -> Creditcard
+type CreditCardC = {
+    type: number;
+    last_four: string;
+}
+
+// Response
+type ResponseMessage = {
+    status?: number,
+    reason?: string,
+}
+
+type ResponseResult<T=any> = { data: T } & ResponseMessage;
+
+// JWT
+type AccessArgs = {
+    userId: string,
+    visitorId: string,
+    browserId: string
+}
+
+type RefreshArgs = {
+    userId: string,
+    visitorId: string,
+    browserId: string
+}
+
+type AccessContent = JwtPayload & {
+    [P in keyof AccessArgs]: AccessArgs[P]
+}
+
+type RefreshContent = JwtPayload & {
+    [P in keyof RefreshArgs]
+}
+
+
+// Router
+// -> Auth
+type SigninRouter = boolean;
+type SignoutRouter = boolean;
+type SignupRouter = { message: FieldMessage }|boolean;
+type RefreshRouter = boolean;
+
+// -> Brand
+type GetallBrandsRouter = Brand[];
+
+// -> Card
+type GetoneCardRouter = CardX|null;
+
+// -> Category
+type GetallCategoriesRouter = Category[];
+
+// -> Collection
+type GetallCollectionsRouter = Collection[];
+
+// -> Cookie
+type DeleteCookieRouter = boolean;
+type GetCookieRouter = string|null;
+type SetCookieRouter = boolean;
+
+// -> Creditcard
+type GetoneCreditcardRouter = CreditCardC|null;
+
+// -> Email
+type SendEmailVerifyEmailRouter = boolean;
+type SendEmailVerifyRequestRouter = boolean;
+type VerifyTokenEmail = boolean;
+type verifyTokenRequest = boolean;
+
+// -> Love
+type ToggleLoveRouter = boolean|null;
+
+// -> Model
+type GetallModelsRouter = Model[];
+
+// -> Order
+type GetManyOrdersRouter = OrderX[];
+
+// -> Product
+type GetOneProductRouter = ProductX|null;
+type GetmanyProductNamesRouter = Suggestion[];
+
+type GetmanyNewProductsRouter = ProductX[];
+type GetmanyNewProductsAllCountRouter = number;
+
+type GetmanyPopularProductsRouter = ProductX[];
+type GetmanyPopularProductsAllCountRouter = number;
+
+type GetmanyRelatedProductsRouter = ProductX[];
+
+type GetmanySearchProductsRouter = ProductX[];
+type GetmanySearchProductsAllCountRouter = number;
+
+type GetmanyToprateProductsRouter = ProductX[];
+type GetmanyToprateProductsAllCountRouter = number;
+
+// -> User
+type UpdateoneBillingRouter = boolean;
+type UpdateoneContactRouter = { message: FieldMessage }|boolean;
+type UpdateoneGeneralRouter = { message: FieldMessage }|boolean;
+type UpdateoneSecurity = boolean;
+type GetoneUserRouter = UserC|null;
+
+// -> CSRF
+type GetCsrfRouter = string;
+
+// General
+type ReleasedAt =
+    'Last hour'|
+    'Yesterday'|
+    'Last week'|
+    'Last month'|
+    'Last year';
+
+type FieldMessage = {
+    field?: string,
+    content: string
+}
+
+type Suggestion = {
+    title: string,
+    searched?: boolean
+}
+
+type FetchResult = {
+    data?: string,
+    reason?: string
+}
+
+
+// Cookie
+type CookieName = 'access-token'|'refresh-token'|'csrf-token';
+
+// Email
+type Email = 'email'|'request'
